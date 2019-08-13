000598fa62aea05c47a85051edcb4b1e8b8110fa
cfiander <cfiander90@gmail.com>
2019-08-12 19:07:45 -0700

    check

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/apihttps://chris-petful-server.herokuapp.com/api',
+    API_ENDPOINT: 'https://chris-petful-server.herokuapp.com/api',
     REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || `http://localhost:8000/api`
 }
